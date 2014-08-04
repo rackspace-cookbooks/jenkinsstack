7e6af6744f590a34e95c3ae06e1016213fc73d5e
Martin Smith <martin@mbs3.org>
2014-08-04 15:53:12 -0500

    Initial commit of very simple, basic Jenkins

---
+++ metadata.rb
@@ -0,0 +1,11 @@
+# Encoding: utf-8
+name             'jenkinsstack'
+maintainer       'Rackspace'
+maintainer_email 'rackspace-cookbooks@rackspace.com'
+license          'Apache 2.0'
+description      'Installs/Configures jenkinsstack'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'jenkins', '~> 1.2'
+depends 'build-essential'

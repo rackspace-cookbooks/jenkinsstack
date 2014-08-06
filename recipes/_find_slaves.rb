c4fd7128e37d35ebb1159e3bb329d43d5fd184cd
Martin Smith <martin@mbs3.org>
2014-08-06 15:30:47 -0500

    Don't provide a slave for default test suite, it won't let Jenkins restart

--- recipes/_find_slaves.rb
+++ recipes/_find_slaves.rb
@@ -11,20 +11,20 @@ include_recipe 'chef-sugar'
 if Chef::Config[:solo]
   errmsg = 'This recipe uses search if slaves attribute is not set. \
    Chef Solo does not support search.'
-  Chef::Application.Log(errmsg)
+  Chef::Log.warn(errmsg)
 else
   slaves = search('node', 'tags:jenkinsstack_slave'\
                   " AND chef_environment:#{node.chef_environment}")
 
   if slaves.nil? || slaves.count == 0
     errmsg = 'Did not find Jenkins slaves to use, but none were set'
-    Chef::Application.Log(errmsg)
+    Chef::Log.warn(errmsg)
   else
     node.set['jenkinsstack']['slaves'] = {}
     slaves.each do |slave|
       best_ip = best_ip_for(slave)
       if best_ip.nil? || best_ip.empty?
-        Chef::Application.warn("$best_ip_for(#{slave.name}) returned bad value")
+        Chef::Log.warn("$best_ip_for(#{slave.name}) returned bad value")
       else
         node.set['jenkinsstack']['slaves'][slave.name] = best_ip
       end

1c322ae263c9c5e8b0270724d98b19e81c426f8e
Martin Smith <martin@mbs3.org>
2014-08-19 10:49:20 -0500

    Additional attribute needed

--- recipes/_base.rb
+++ recipes/_base.rb
@@ -22,13 +22,15 @@ node.default['nginx']['default_site_enabled'] = false
 node.default['jenkins']['master']['jvm_options'] = '-XX:MaxPermSize=512m'
 node.default['jenkins']['master']['listen_address'] = '127.0.0.1'
 
-# The following attributes need to match.  The first is the jenkins.war version to download from
-# http://mirrors.jenkins-ci.org/war/ and the second is the sha256 hash of the file to prevent download
+# The following attributes need to match.  They are the jenkins.war version to download from
+# http://mirrors.jenkins-ci.org/war/ and the sha256 hash of the file to prevent download
 # of the war file on every Chef run.
 # comment out old version --
+node.default['jenkins']['master']['install_method'] = 'war'
 node.default['jenkins']['master']['version']      = '1.555'
 node.default['jenkins']['master']['checksum'] = '31f5c2a3f7e843f7051253d640f07f7c24df5e9ec271de21e92dac0d7ca19431'
-node.default['jenkins']['master']['install_method'] = 'war'
+node.default['jenkins']['master']['source'] = "#{node['jenkins']['master']['mirror']}/war/#{node['jenkins']['master']['version']}/jenkins.war"
+
 
 node['jenkinsstack']['packages'].each do |pkg|
   package pkg do

e226a22bc06ddcdea00802ffbf6b1b3231a0db80
Julien Berard <jujugrrr@gmail.com>
2015-03-31 15:05:04 +0100

    Changed source path for Jenkins.war

--- recipes/_base.rb
+++ recipes/_base.rb
@@ -21,14 +21,14 @@ node.default['nginx']['default_site_enabled'] = false
 node.default['jenkins']['master']['jvm_options'] = '-XX:MaxPermSize=512m'
 node.default['jenkins']['master']['listen_address'] = '127.0.0.1'
 
-# The following attributes need to match.  They are the jenkins.war version to download from
+# The following attributes need to match. They are the jenkins.war version to download from
 # http://mirrors.jenkins-ci.org/war/ and the sha256 hash of the file to prevent download
 # of the war file on every Chef run.
 # comment out old version --
 node.default['jenkins']['master']['install_method'] = 'war'
 node.default['jenkins']['master']['version']      = '1.555'
 node.default['jenkins']['master']['checksum'] = '31f5c2a3f7e843f7051253d640f07f7c24df5e9ec271de21e92dac0d7ca19431'
-node.default['jenkins']['master']['source'] = "#{node['jenkins']['master']['mirror']}/war/#{node['jenkins']['master']['version']}/jenkins.war"
+node.default['jenkins']['master']['source'] = 'http://d150860c924442f0c4ba-bb788a6a02be77ad65ac0686e629b047.r71.cf3.rackcdn.com/packages/jenkins/1.555/jenkins.war'
 
 node['jenkinsstack']['packages'].each do |pkg|
   package pkg do
